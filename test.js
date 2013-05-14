bdddd9cb833974f22fb054834c114748ae45efd4
Andreas Madsen <amwebdk@gmail.com>
2013-05-14 19:32:16 +0200

    [init] commit

---
+++ test.js
@@ -0,0 +1,39 @@
+
+var test = require('tap').test;
+var worddiff = require('./worddiff.js');
+
+test('all match', function (t) {
+  var diff = worddiff('hallo world', 'hallo world');
+  t.equal(diff, 0);
+  t.end();
+});
+
+test('left has extra', function (t) {
+  var diff = worddiff('hallo world extra', 'hallo world');
+  t.equal(diff, 1);
+  t.end();
+});
+
+test('right has extra', function (t) {
+  var diff = worddiff('hallo world', 'hallo world extra');
+  t.equal(diff, 1);
+  t.end();
+});
+
+test('left is empty', function (t) {
+  var diff = worddiff('', 'hallo world');
+  t.equal(diff, 2);
+  t.end();
+});
+
+test('right is empty', function (t) {
+  var diff = worddiff('hallo world', '');
+  t.equal(diff, 2);
+  t.end();
+});
+
+test('extra space do not matter', function (t) {
+  var diff = worddiff('  hallo  \t\r \n  world', 'hallo world  ');
+  t.equal(diff, 0);
+  t.end();
+});
---
+++ worddiff.js
@@ -0,0 +1,33 @@
+
+var SPACE = /\s+/;
+
+function split(s) {
+  s = s.split(SPACE);
+  if (s[0] === '') s = s.slice(1);
+  if (s[s.length - 1] === '') s = s.slice(0, -1);
+
+  return s;
+}
+
+function worddiff(a, b) {
+  a = split(a);
+  b = split(b);
+  
+  console.log(a, b);
+  
+  // Remove items there match from both a and b
+  var i = 0;
+  while (i < b.length && a.length > 0) {
+    var index = a.indexOf(b[i]);
+    if (index !== -1) {
+      a.splice(index, 1);
+      b.splice(i, 1);
+    } else {
+      i += 1;
+    }
+  }
+
+  // Only unique items should exists now, so the element sum will be the diff
+  return a.length + b.length;
+}
+module.exports = worddiff;
